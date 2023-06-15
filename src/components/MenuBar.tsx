618f2162127772ad87d2f2b6424ffe4dbf6b6dfe
kage1414 <kylejohnson92294@gmail.com>
2023-06-15 16:51:47 -0400

    Move tabs to i18n

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,8 +1,10 @@
 import { Box, AppBar, Typography, Toolbar, Button } from "@mui/material";
 import Link from "next/link";
 import { ReactElement } from "react";
+import { useTranslation } from "react-i18next";
 
 export const MenuBar = (): ReactElement => {
+  const { t } = useTranslation("common");
   return (
     <Box>
       <AppBar
@@ -28,22 +30,22 @@ export const MenuBar = (): ReactElement => {
             <Box display="flex" flexWrap={"wrap"}>
               <Link href={"/"}>
                 <Button sx={{ color: "white" }} disableRipple>
-                  <Typography padding={1}>Home</Typography>
+                  <Typography padding={1}>{t("home")}</Typography>
                 </Button>
               </Link>
               <Link href={"/order"}>
                 <Button sx={{ color: "white" }} disableRipple>
-                  <Typography padding={1}>Order</Typography>
+                  <Typography padding={1}>{t("order")}</Typography>
                 </Button>
               </Link>
               <Link href={"/about"}>
                 <Button sx={{ color: "white" }} disableRipple>
-                  <Typography padding={1}>About</Typography>
+                  <Typography padding={1}>{t("about")}</Typography>
                 </Button>
               </Link>
               <Link href={"/photos"}>
                 <Button sx={{ color: "white" }} disableRipple>
-                  <Typography padding={1}>Photos</Typography>
+                  <Typography padding={1}>{t("photos")}</Typography>
                 </Button>
               </Link>
             </Box>
