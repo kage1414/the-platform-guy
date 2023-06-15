d5d44096c5e36e34e21268a9080f7c8986391346
kage1414 <kylejohnson92294@gmail.com>
2023-06-15 16:40:36 -0400

    Remove webpack5

--- next.config.js
+++ next.config.js
@@ -4,10 +4,8 @@ const { i18n } = require("./next-i18next.config.js");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  webpack5: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
     return config;
   },
 };
